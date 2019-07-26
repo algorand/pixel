1968ece071d1bacf6cfe9bbac9c2bed283fe6192
zhenfei <zhenfei.zhang@hotmail.com>
2019-07-26 08:07:05 -0400

    removing deprecated tests

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,12 +1,5 @@
 extern crate rand;
 
-/// This module contains deterministic tests, with pre-fixed parameters,
-/// and with determinstic, small random numbers, e.g., 1, 2, 3, 4...
-/// This test module is only avaliable when public key lies in G2.
-#[cfg(debug_assertions)]
-#[cfg(feature = "pk_in_g2")]
-mod det_test;
-
 /// This module tests basic API's
 mod api;
 

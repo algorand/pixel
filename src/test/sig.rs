b5bebb012b31e260ecb07c678c2d85b4153196e9
zhenfei <zhenfei.zhang@hotmail.com>
2019-08-06 15:23:57 -0400

    some must-fail tests

--- src/test/sig.rs
+++ src/test/sig.rs
@@ -1,8 +1,124 @@
 use key_pair::KeyPair;
 use param::PubParam;
+use pixel_err;
 use sig::Signature;
 use PublicKey;
 use SecretKey;
+
+/// Must fail tests for signatures
+#[test]
+fn test_must_fails_seed() {
+    let res = PubParam::init(b"seed", 0);
+    res.expect_err(pixel_err::ERR_SEED_TOO_SHORT);
+
+    let pp = PubParam::default();
+    let res = KeyPair::keygen(b"", &pp);
+    res.expect_err(pixel_err::ERR_SEED_TOO_SHORT);
+}
+
+#[test]
+fn test_must_fails_mismatch() {
+    let pp1 = PubParam::default();
+    let pp2 = pp1.clone();
+    let pp3 = PubParam::init(b"this is a very very long seed for testing", 0).unwrap();
+
+    let msg1 = b"message to sign";
+    let msg2 = b"anther message to sign";
+
+    let (pk1, sk1, pop1) =
+        KeyPair::keygen(b"this is a very very long seed for testing", &pp1).unwrap();
+
+    let (pk2, sk2, pop2) =
+        KeyPair::keygen(b"this is another very very long seed for testing", &pp2).unwrap();
+
+    let (pk3, _sk3, pop3) =
+        KeyPair::keygen(b"this is a third very very long seed for testing", &pp3).unwrap();
+
+    // use popi to validate pkj with i != j
+    assert!(pk1.validate(&pop1));
+    assert!(!pk1.validate(&pop2));
+    assert!(!pk1.validate(&pop3));
+    assert!(pk2.validate(&pop2));
+    assert!(!pk2.validate(&pop1));
+    assert!(!pk2.validate(&pop3));
+    assert!(pk3.validate(&pop3));
+    assert!(!pk3.validate(&pop1));
+    assert!(!pk3.validate(&pop2));
+
+    // mis-match the pk, sk, msg, pp, verification should fail
+    let res = Signature::sign_bytes(&sk1, 1, &pp1, msg1);
+    assert!(res.is_ok(), "signing failed");
+    let sig11 = res.unwrap();
+
+    let res = Signature::sign_bytes(&sk1, 1, &pp1, msg2);
+    assert!(res.is_ok(), "signing failed");
+    let sig12 = res.unwrap();
+
+    assert!(sig11.verify_bytes(&pk1, &pp1, msg1));
+    assert!(!sig11.verify_bytes(&pk2, &pp1, msg1));
+    assert!(!sig11.verify_bytes(&pk1, &pp3, msg1));
+    assert!(!sig11.verify_bytes(&pk1, &pp1, msg2));
+    assert!(!sig12.verify_bytes(&pk1, &pp1, msg1));
+
+    let res = Signature::sign_bytes(&sk2, 1, &pp1, msg1);
+    assert!(res.is_ok(), "signing failed");
+    let sig21 = res.unwrap();
+
+    let res = Signature::sign_bytes(&sk2, 1, &pp1, msg2);
+    assert!(res.is_ok(), "signing failed");
+    let sig22 = res.unwrap();
+
+    assert!(sig21.verify_bytes(&pk2, &pp2, msg1));
+    assert!(!sig21.verify_bytes(&pk1, &pp1, msg1));
+    assert!(!sig21.verify_bytes(&pk1, &pp3, msg1));
+    assert!(!sig21.verify_bytes(&pk1, &pp1, msg2));
+    assert!(!sig22.verify_bytes(&pk1, &pp1, msg1));
+}
+
+#[test]
+fn test_must_fails_time_stamp() {
+    let pp = PubParam::default();
+
+    let msg = b"message to sign";
+
+    let (_pk, mut sk, _pop) =
+        KeyPair::keygen(b"this is a very very long seed for testing", &pp).unwrap();
+    let mut sk2 = sk.clone();
+
+    // sign for an invalid time stamp
+    let res = Signature::sign_present(&mut sk, 2, &pp, msg);
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+
+    assert!(sk2.update(&pp, 10, b"").is_ok());
+
+    // sign for present
+    let res = Signature::sign_present(&mut sk2, 0, &pp, msg);
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+
+    let res = Signature::sign_present(&mut sk2, 9, &pp, msg);
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+
+    let res = Signature::sign_present(&mut sk2, 11, &pp, msg);
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+
+    // sign for present and future
+    let res = Signature::sign(&mut sk2, 0, &pp, msg, b"");
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+
+    let res = Signature::sign(&mut sk2, 9, &pp, msg, b"");
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+
+    let res = Signature::sign(&mut sk2, 10, &pp, msg, b"");
+    assert!(res.is_ok());
+
+    // update sk2 to an invalid time stamp
+    let res = sk2.update(&pp, 0, b"");
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+    let res = sk2.update(&pp, 9, b"");
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+    let res = sk2.update(&pp, 10, b"");
+    res.expect_err(pixel_err::ERR_TIME_STAMP);
+}
 
 /// A simple and quick tests on
 /// * key generation
